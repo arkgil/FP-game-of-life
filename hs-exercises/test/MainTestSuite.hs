7455ef9bce6b6573d5893e56e06032e36aaef366
Arkadiusz Zając <arkadiusz.z@outlook.com>
2017-06-07 10:19:15 +0200

    Refactor and fix warnings in tests

--- hs-exercises/test/MainTestSuite.hs
+++ hs-exercises/test/MainTestSuite.hs
@@ -14,7 +14,6 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-
 tests = testGroup "Tests" [functors, applicatives, monads]
 
 functors =
@@ -22,15 +21,15 @@ functors =
     [ testProperty "Functor preserves identity morphism"        propPreserveIdentityMorphism
     , testProperty "Functor preserves composition of morphisms" propPreserveCompositionOfMorphisms ]
 
+applicatives =
+    testGroup "Applicatives"
+    [ testCase "Identity of list applicative"     listApplicativeIdentity
+    , testCase "Homomorphism of list applicative" listApplicativeHomomorphism
+    , testCase "Interchange of list applicative"  listApplicativeInterchange
+    , testCase "Composition of list applicative"  listApplicativeComposition ]
+
 monads =
     testGroup "Monads"
     [ testProperty "Left identity"  propMonadLeftIdentity
     , testProperty "Right identity" propMonadRightIdentity
     , testProperty "Associativity"  propMonadAssociativity ]
-
-applicatives =
-  testGroup "Applicatives"
-  [testCase     "Identity of list applicative"      listApplicativeIdentity
-  , testCase     "Homomorphism of list applicative" listApplicativeHomomorphism
-  , testCase     "Interchange of list applicative"  listApplicativeInterchange
-  , testCase     "Composition of list applicative"  listApplicativeComposition ]
